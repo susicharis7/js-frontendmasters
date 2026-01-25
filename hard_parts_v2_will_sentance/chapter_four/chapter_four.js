276e326e40805dc5e8ca21dceb55a87f69c41e4c
susicharis7 <susicharis99@gmail.com>
2026-01-25 19:55:05 +0100

    Finished Chapter Four

--- hard_parts_v2_will_sentance/chapter_four/chapter_four.js
+++ hard_parts_v2_will_sentance/chapter_four/chapter_four.js
@@ -21,3 +21,41 @@ async function getUserPostStats() {
 
 
 getUserPostStats().then(result => console.log(result));
+
+
+
+
+
+/* Now we know that JS Code is not all "JS", a lot of it is from WEB API, ect.. */
+function printHello() {
+    console.log("Hello");
+}
+
+/* setTimeout is not JS function, it is a Web Browser feature that is injected into JS */
+setTimeout(printHello, 1000);
+console.log("Me First")
+
+
+function printHi() {
+    console.log("Hi");
+}
+
+function blockForOneSec() {
+    // blocks in the JS thread for 1 sec
+}
+
+setTimeout(printHi, 0);
+blockForOneSec();
+console.log("Me first!");
+
+
+
+/* Background feature callbacks are challenging for data handling */
+let data;
+
+setTimeout(() => {
+    return 42; 
+}, 1000);
+
+console.log(data); // -> UNDEFINED 
+/* It is undefined because the return in setTimeout means WEB API will get it, not JS */
