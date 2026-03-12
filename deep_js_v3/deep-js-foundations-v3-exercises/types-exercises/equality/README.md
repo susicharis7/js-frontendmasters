53bb7162ed7cfcde7651fd5547c240235d4f5c05
susicharis7 <susicharis99@gmail.com>
2026-03-12 20:29:58 +0100

    Equality Task Completed

--- deep_js_v3/deep-js-foundations-v3-exercises/types-exercises/equality/README.md
+++ deep_js_v3/deep-js-foundations-v3-exercises/types-exercises/equality/README.md
@@ -8,9 +8,22 @@ In this exercise, you will define a `findAll(..)` function that searches an arra
 
 2. The coercive matching that is allowed:
 
-	- exact matches (`Object.is(..)`)
+- exact matches (`Object.is(..)`) 
 	- strings (except "" or whitespace-only) can match numbers
 	- numbers (except `NaN` and `+/- Infinity`) can match strings (hint: watch out for `-0`!)
-	- `null` can match `undefined`, and vice versa
-	- booleans can only match booleans
-	- objects only match the exact same object
+- `null` can match `undefined`, and vice versa
+- booleans can only match booleans
+- objects only match the exact same object
+
+
+# My helpers
+
+findAll()
+- treba proci kroz svaki element u array 
+- odluciti da li se taj element "coercively matches" sa value
+- ako se matchuje -> dodati ga u rezultat 
+
+console.log(setsMatch(findAll(-0,values),[-0]) === true);
+- u setsMatch salje 2 argumenta da provjeri da li 2 arraya imaju iste elemente
+- prvi argument je rezultat od findAll funkcije, a drugi argument je [-0]
+- ako vrati true, provjerava onda sa `=== true` i dobiti cemo true iz console loga
--- deep_js_v3/deep-js-foundations-v3-exercises/types-exercises/equality/ex.js
+++ deep_js_v3/deep-js-foundations-v3-exercises/types-exercises/equality/ex.js
@@ -1,6 +1,41 @@
 // TODO: write `findAll(..)`
+function findAll(value, array) {
+	// napraviti prazan result 
+	let result = [];
+
+	// proci kroz svaki element iz `array`
+	// ako element matchuje sa value, ubaciti ga u rezultat
+	for (let v of array) {
+
+		if (Object.is(v, value)) {
+			result.push(v);
+		} else if (value === null && v === undefined || value === undefined && v === null) {
+			result.push(v);
+		} else if (typeof value == "string" && typeof v == "number" && value.trim() != "" && !Object.is(-0, v)) {
+			if (value == v) {
+				result.push(v);
+			};
+		} else if (typeof value == "number" && typeof v == "string" && !Object.is(value, -0) && !Object.is(value, NaN) && !Object.is(value, -Infinity) && !Object.is(value, Infinity) && v.trim() != "") {
+			if (value == v) {
+				result.push(v);
+			};
+		}
+		
 
 
+	
+
+		
+	};
+
+	
+
+
+
+
+	// na kraju vratiti rezultat 
+	return result; 
+};
 
 // tests:
 var myObj = { a: 2 };
@@ -10,23 +45,23 @@ var values = [
 	"", "0", "42", "42hello", "true", "NaN", true, false, myObj
 ];
 
-console.log(setsMatch(findAll(null,values),[null,undefined]) === true);
-console.log(setsMatch(findAll(undefined,values),[null,undefined]) === true);
-console.log(setsMatch(findAll(0,values),[0,"0"]) === true);
 console.log(setsMatch(findAll(-0,values),[-0]) === true);
 console.log(setsMatch(findAll(13,values),[13]) === true);
-console.log(setsMatch(findAll(42,values),[42,"42"]) === true);
 console.log(setsMatch(findAll(NaN,values),[NaN]) === true);
 console.log(setsMatch(findAll(-Infinity,values),[-Infinity]) === true);
 console.log(setsMatch(findAll(Infinity,values),[Infinity]) === true);
 console.log(setsMatch(findAll("",values),[""]) === true);
-console.log(setsMatch(findAll("0",values),[0,"0"]) === true);
-console.log(setsMatch(findAll("42",values),[42,"42"]) === true);
 console.log(setsMatch(findAll("42hello",values),["42hello"]) === true);
 console.log(setsMatch(findAll("true",values),["true"]) === true);
 console.log(setsMatch(findAll(true,values),[true]) === true);
 console.log(setsMatch(findAll(false,values),[false]) === true);
 console.log(setsMatch(findAll(myObj,values),[myObj]) === true);
+console.log(setsMatch(findAll(null,values),[null,undefined]) === true);
+console.log(setsMatch(findAll(undefined,values),[null,undefined]) === true);
+console.log(setsMatch(findAll(0,values),[0,"0"]) === true);
+console.log(setsMatch(findAll(42,values),[42,"42"]) === true);
+console.log(setsMatch(findAll("0",values),[0,"0"]) === true);
+console.log(setsMatch(findAll("42",values),[42,"42"]) === true);
 
 console.log(setsMatch(findAll(null,values),[null,0]) === false);
 console.log(setsMatch(findAll(undefined,values),[NaN,0]) === false);
@@ -42,6 +77,7 @@ console.log(setsMatch(findAll(false,values),[false,0]) === false);
 
 // ***************************
 
+// Da li 2 arraya imaju iste elemente 
 function setsMatch(arr1,arr2) {
 	if (Array.isArray(arr1) && Array.isArray(arr2) && arr1.length == arr2.length) {
 		for (let v of arr1) {
